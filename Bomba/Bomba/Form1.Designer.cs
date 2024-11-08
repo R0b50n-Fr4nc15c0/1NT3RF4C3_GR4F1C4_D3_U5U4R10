9514149b5f332077f6f212173bc64bb710017a99
R0b50n-Fr4nc15c0 <robson.ninecraft@gmail.com>
2024-11-08 14:17:01 -0300

    Add files via upload

---
+++ Bomba/Bomba/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Bomba
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            buttonAbrir = new Button();
+            buttonSalvar = new Button();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            SuspendLayout();
+            // 
+            // buttonAbrir
+            // 
+            buttonAbrir.Location = new Point(12, 8);
+            buttonAbrir.Name = "buttonAbrir";
+            buttonAbrir.Size = new Size(155, 23);
+            buttonAbrir.TabIndex = 0;
+            buttonAbrir.Text = "Abrir";
+            buttonAbrir.UseVisualStyleBackColor = true;
+            buttonAbrir.Click += buttonAbrir_Click;
+            // 
+            // buttonSalvar
+            // 
+            buttonSalvar.Location = new Point(173, 8);
+            buttonSalvar.Name = "buttonSalvar";
+            buttonSalvar.Size = new Size(155, 23);
+            buttonSalvar.TabIndex = 1;
+            buttonSalvar.Text = "Salvar";
+            buttonSalvar.UseVisualStyleBackColor = true;
+            buttonSalvar.Click += buttonSalvar_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(431, 12);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 2;
+            label1.Text = "label1";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 37);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(519, 322);
+            textBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(543, 371);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(buttonSalvar);
+            Controls.Add(buttonAbrir);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button buttonAbrir;
+        private Button buttonSalvar;
+        private Label label1;
+        private TextBox textBox1;
+    }
+}
---
+++ Bomba/Bomba/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bomba
+{
+    public partial class Form1 : Form
+    {
+        string filename { get; set; }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSalvar_Click(object sender, EventArgs e)
+        {
+            File.WriteAllText(filename, textBox1.Text);
+        }
+
+        private void buttonAbrir_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            var resultado = ofd.ShowDialog(this);
+            filename = ofd.FileName;
+            if (resultado == DialogResult.OK)
+            {
+                string text = File.ReadAllText(filename);
+                textBox1.Text = text;
+                label1.Text = filename;
+            }
+        }
+    }
+}
